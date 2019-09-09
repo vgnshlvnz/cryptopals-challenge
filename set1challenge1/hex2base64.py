1ddb8b5197636b746a503865237dc6320e4b5882
vgnshlvnz <vigneshwaranr@outlook.com>
2019-09-09 15:01:27 +0800

    added Set 1 Challenge 1

---
+++ set1challenge1/hex2base64.py
@@ -0,0 +1,7 @@
+import base64
+
+hexStr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+print(base64.b64encode(bytes.fromhex(hexStr)).decode('utf-8'))
+
