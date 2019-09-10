7cb5834a2688cfb06cfd608d1808186e63a6b95b
vgnshlvnz <vigneshwaranr@outlook.com>
2019-09-10 12:22:22 +0800

    modified module

--- set1challenge1/hex2base64.py
+++ set1challenge1/hex2base64.py
@@ -1,12 +1,14 @@
+from base64 import b64encode as base64Encode
 import base64
+
 
 hexStr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-print(base64.b64encode(bytes.fromhex(hexStr)).decode('utf-8'))
+print(base64Encode(bytes.fromhex(hexStr)).decode('utf-8'))
 
 def hex2base64BaselineCalc():
-    return base64.b64encode(bytes.fromhex(hexStr))
+    return base64Encode(bytes.fromhex(hexStr))
 
 
 def hex2base64(inputStr):
@@ -16,5 +18,5 @@ def hex2base64(inputStr):
         hex2base64BaselineCalc()
     else:
         tempBytes = bytes.fromhex(inputStr)
-        base64Str = base64.b64encode(tempBytes)
+        base64Str = base64Encode(tempBytes)
         return base64Str
