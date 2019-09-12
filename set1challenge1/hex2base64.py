b20ab58b7cc92b7af2aa427a17cc479083254a91
vgnshlvnz <vigneshwaranr@outlook.com>
2019-09-12 13:56:36 +0800

    added print statement for argument more than 2

--- set1challenge1/hex2base64.py
+++ set1challenge1/hex2base64.py
@@ -1,7 +1,7 @@
 ''' Import base64 encoding function from base64 module 
 '''
 from base64 import b64encode as ncode2base64
-
+from sys import argv as argument
 
 hexStr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
@@ -30,7 +30,10 @@ def hex2base64(inputStr):
 
 
 def main():
-    output = hex2base64()
+    if argument >= 2:
+        print("argument more than 2, unable to process further.")
+    else:
+        output = hex2base64(str(argument[1]))
     print(output)
 
 
